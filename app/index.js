2029087662ca9862fddd14a596c6f593c2765ea1
skircher <codeshaghennessy@gmail.com>
2016-01-22 17:25:52 -0600

    added Griddle to see how to use it

--- app/index.js
+++ app/index.js
@@ -7,6 +7,7 @@ import ReactDOM from "react-dom";
 import { Block, Flex } from 'jsxstyle';
 import Text from "./components/text";
 import Counter from "./components/counter";
+import Griddle from 'griddle-react';
 
 import {createStore} from "redux";
 
@@ -55,6 +56,70 @@ const Center = ({ children }) =>
 //   }
 // }
 const store = createStore(counter);
+var teams = [
+  {
+    id:0,
+    name:"team 1",
+    members: [
+      {
+        id:1,
+        name:"Sam",
+        lname:"Iam",
+        compentencyId: 1
+      },
+      {
+        id:2,
+        name:"Pam",
+        lname:"youwho",
+        compentencyId: 2
+      }
+    ]
+  },
+  {
+    id:1,
+    name:"team 2",
+    members : [
+      {
+        id:3,
+        name:"Jim",
+        lname:"Kim",
+        compentencyId: 3
+      },
+      {
+        id:4,
+        name:"Ken",
+        lname:"Gym",
+        compentencyId: 2
+      }
+    ]
+  }
+]
+var fakeData =  [
+{
+  "January": 35,
+  "February": 20,
+  "March": 27,
+  "April": 32,
+  "May": 23,
+  "June": 42
+},
+{
+  "January": 35,
+  "February": 20,
+  "March": 27,
+  "April": 32,
+  "May": 23,
+  "June": 42
+},
+{
+  "January": 35,
+  "February": 20,
+  "March": 27,
+  "April": 32,
+  "May": 23,
+  "June": 42
+}
+];
 
 class Application extends React.Component {
   render() {
@@ -65,6 +130,7 @@ class Application extends React.Component {
           <Text value="try"/>
           <Text value="test"/>
           <Center>
+          <div>
           <Counter value={store.getState()}
             onInc={() =>
              store.dispatch({
@@ -77,6 +143,8 @@ class Application extends React.Component {
            })
           }
           />
+          </div>
+          <Griddle childrenColumnName="members" results={teams}/>
           </Center>
           </Fonts>
 
