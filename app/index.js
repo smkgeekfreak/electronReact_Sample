55d602510801bc18033b370f1ae42c5b47f38674
Shawn Kircher <smkgeekfreak@gmail.com>
2016-02-15 18:27:47 -0600

    cleanup commented out code

--- app/index.js
+++ app/index.js
@@ -17,42 +17,6 @@ import {createStore, applyMiddleware, compose} from "redux";
 
 import todoApp from './containers/todos';
 
-// const counter = (state = 0, action) => {
-//   switch (action.type) {
-//     case 'INC':
-//       return state + 1;
-//     case "DEC" :
-//       return state- 1;
-//     default:
-//       return state;
-//   }
-// };
-//
-// const filter = (state='ALL', action) => {
-//   switch (action.type) {
-//     case 'SET_FILTER':
-//       return action.filter;
-//     default:
-//       return state;
-//   }
-// }
-//
-// const rootStore =(state={}, action) => {
-//   return {
-//     counter: counter(state, action),
-//     filter: filter(state.filter,action)
-//   };
-// };
-
-// const finalCreateStore = compose(
-//   // Middleware you want to use in development:
-//   // Required! Enable Redux DevTools with the monitors you chose
-//   DevTools.instrument()
-//   // Optional. Lets you write ?debug_session=<key> in address bar to persist debug sessions
-//   // persistState(getDebugSessionKey())
-// )(createStore);
-
-// const store = finalCreateStore(todoApp);
 const store = createStore(todoApp);
 
 console.log("Initial State");
